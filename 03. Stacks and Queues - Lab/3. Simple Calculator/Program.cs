b9545cc1af0f2ff072fad40af8c0c9ec723a4734
wirert <wirert@students.softuni.bg>
2022-09-14 20:42:44 +0300

    third task

---
+++ 03. Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Reverse();
+
+            var stack = new Stack<string>(input);
+            int sum = int.Parse(stack.Pop());
+            while (stack.Count != 0)
+            {
+                var action = stack.Pop();
+
+                if (action == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
