1ae5239e6cbceb299c6c904f9b23074184ed69ff
wirert <wirert@students.softuni.bg>
2022-09-14 19:40:27 +0300

    first task

--- 03. Stacks and Queues - Lab/1. Reverse a String/Program.cs
+++ 03. Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _1._Reverse_a_String
 {
@@ -6,7 +7,20 @@ namespace _1._Reverse_a_String
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
         }
     }
 }
