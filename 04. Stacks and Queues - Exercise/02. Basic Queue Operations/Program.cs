1be1ed2a616766563e4fc8aee98afcfd4dfbc5c7
wirert <wirert@students.softuni.bg>
2022-09-16 17:51:51 +0300

    task 2

---
+++ 04. Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cmd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            for (int i = 0; i < cmd[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (queue.Contains(cmd[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
