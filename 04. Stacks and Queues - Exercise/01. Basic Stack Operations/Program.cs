b8ef635a7a8a2916e73415c88ae1bf12916ad8fb
wirert <wirert@students.softuni.bg>
2022-09-16 17:48:14 +0300

    task 1

---
+++ 04. Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cmd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var stack = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            for (int i = 0; i < cmd[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (stack.Contains(cmd[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
