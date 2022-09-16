c17bd601e101a8a1971050b358cf80778a84448f
wirert <wirert@students.softuni.bg>
2022-09-16 16:19:27 +0300

    task 4

---
+++ 03. Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var openBrackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    openBrackets.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int index = openBrackets.Pop();
+                    Console.WriteLine(input.Substring(index, i - index + 1));
+                }
+            }
+        }
+    }
+}
