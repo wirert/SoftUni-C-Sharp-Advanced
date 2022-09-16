27e7155c0292316517647a05a7bb018184d3d45a
wirert <wirert@students.softuni.bg>
2022-09-16 17:03:28 +0300

    task 7

---
+++ 03. Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>(Console.ReadLine().Split());
+            int tossCount = int.Parse(Console.ReadLine());
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i <= tossCount; i++)
+                {
+                    if (i == tossCount)
+                    {
+                        Console.WriteLine($"Removed {queue.Dequeue()}");
+                    }
+                    else
+                    {
+                        string kid = queue.Dequeue();
+                        queue.Enqueue(kid);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
