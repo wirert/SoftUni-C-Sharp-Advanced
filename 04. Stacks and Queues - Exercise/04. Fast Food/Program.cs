8c77af5e9e4c8f427a2e104b0e6103677d43f6fa
wirert <wirert@students.softuni.bg>
2022-09-16 20:44:07 +0300

    task 4

---
+++ 04. Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQnt = int.Parse(Console.ReadLine());
+
+            var ordersQueue = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            Console.WriteLine(ordersQueue.Max());
+            while (ordersQueue.Count != 0)
+            {
+                if (ordersQueue.Peek() > foodQnt)
+                {
+                    Console.WriteLine($"Orders left: {String.Join(" ", ordersQueue)}");
+                    return;
+                }
+
+                foodQnt -= ordersQueue.Dequeue();
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
