7c0e8f0174cffd0ab649b6327dbd99058d4aef64
wirert <wirert@students.softuni.bg>
2022-09-16 16:43:24 +0300

    task 6

---
+++ 03. Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+
+            var queue = new Queue<string>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input != "Paid")
+                {
+                    queue.Enqueue(input);
+                }
+                else
+                {
+                    while (queue.Any())
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
