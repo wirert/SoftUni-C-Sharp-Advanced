84ba544e61c286f9aaee8377e74f27239a613189
wirert <wirert@students.softuni.bg>
2022-09-16 16:30:17 +0300

    task 5

---
+++ 03. Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>();
+
+            foreach (var curNum in nums)
+            {
+                if (curNum % 2 == 0)
+                {
+                    queue.Enqueue(curNum);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
