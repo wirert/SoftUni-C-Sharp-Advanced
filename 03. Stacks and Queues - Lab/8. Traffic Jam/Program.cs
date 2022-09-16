804ac223e9674ef1225795dffb9a10ec608039ba
wirert <wirert@students.softuni.bg>
2022-09-16 17:16:23 +0300

    task 8

---
+++ 03. Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsToPass = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            string command = null;
+            int count = 0;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command != "green")
+                {
+                    queue.Enqueue(command);
+                }
+                else
+                {
+                    for (int i = 1; i <= carsToPass; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
