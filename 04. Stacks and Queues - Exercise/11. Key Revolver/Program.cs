69d5e8b8d0458c14dff73c0ca5935d3db76ff664
wirert <wirert@students.softuni.bg>
2022-09-20 17:09:08 +0300

    task 11

---
+++ 04. Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            var bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int plunder = int.Parse(Console.ReadLine());
+            int shotBullets = 0;
+
+            while (bullets.Any() && locks.Any())
+            {
+
+                if (bullets.Pop() <= locks.Peek())
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                shotBullets++;
+
+                if (shotBullets % barrelSize == 0 && bullets.Any())
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            if (locks.Any())
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                plunder -= shotBullets * bulletPrice;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${plunder}");
+            }
+        }
+    }
+}
