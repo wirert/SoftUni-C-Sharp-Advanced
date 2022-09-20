81666ee23068915d0437222af104378da812f837
wirert <wirert@students.softuni.bg>
2022-09-20 17:56:16 +0300

    task 12

---
+++ 04. Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int waterWasted = 0;
+
+            int cup = cups.Dequeue();
+
+            while (bottles.Any())
+            {
+                int bottle = bottles.Pop();
+
+                if (bottle >= cup)
+                {
+                    waterWasted += bottle - cup;
+
+                    if (!cups.Any() || !bottles.Any()) break;
+
+                    cup = cups.Dequeue();
+                }
+                else
+                {
+                    cup -= bottle;
+                }
+            }
+
+            Console.WriteLine(bottles.Any()
+                ? $"Bottles: {String.Join(" ", bottles)}"
+                : $"Cups: {String.Join(" ", cups)}");
+
+            Console.WriteLine($"Wasted litters of water: {waterWasted}");
+        }
+    }
+}
