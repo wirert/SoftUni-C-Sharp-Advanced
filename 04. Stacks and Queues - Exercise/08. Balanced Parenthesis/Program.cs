346c591bf1c31d94f386d0d1b5428695dcb8efb5
wirert <wirert@students.softuni.bg>
2022-09-20 13:28:46 +0300

    task 8

---
+++ 04. Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            var openBrackets = new Stack<char>(input);
+            bool isBalanced = true;
+
+            foreach (char bracket in input)
+            {
+                if (bracket == '{' || bracket == '[' || bracket == '(')
+                {
+                    openBrackets.Push(bracket);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    if ((bracket == '}' && openBrackets.Peek() == '{')
+                        || (bracket == ']' && openBrackets.Peek() == '[')
+                        || (bracket == ')' && openBrackets.Peek() == '('))
+                    {
+                        openBrackets.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(isBalanced ? "YES" : "NO");
+        }
+    }
+}
