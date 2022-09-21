347552d4d7e9b93e108f899b35e1072f1823aee5
wirert <wirert@students.softuni.bg>
2022-09-21 13:07:55 +0300

    task 1

---
+++ 6. Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] inputRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            int firstDiagonal = 0;
+            int secondDiagonal = 0;
+
+            for (int i = 0, j = size - 1; i < size; i++, j--)
+            {
+                firstDiagonal += matrix[i, i];
+                secondDiagonal += matrix[i, j];
+            }
+
+            Console.WriteLine(Math.Abs(firstDiagonal - secondDiagonal));
+        }
+    }
+}
