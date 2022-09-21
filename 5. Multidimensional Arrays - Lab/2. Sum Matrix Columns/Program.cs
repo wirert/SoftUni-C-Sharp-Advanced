5a647d8083b3d21526f81b8283ec741bfd115617
wirert <wirert@students.softuni.bg>
2022-09-21 11:01:49 +0300

    task 2

---
+++ 5. Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int row = 0; row < size[0]; row++)
+            {
+                int[] inputRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            for (int col = 0; col < size[1]; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < size[0]; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
