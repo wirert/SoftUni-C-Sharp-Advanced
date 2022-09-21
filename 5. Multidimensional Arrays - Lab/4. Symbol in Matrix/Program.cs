9a4f0682ff6997ccaf5c94662c914d86ebebf184
wirert <wirert@students.softuni.bg>
2022-09-21 11:31:11 +0300

    task 4

---
+++ 5. Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string inputRow = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            char charToLookFor = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] != charToLookFor) continue;
+
+                    Console.WriteLine($"({row}, {col})");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{charToLookFor} does not occur in the matrix");
+        }
+    }
+}
