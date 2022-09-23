a9d92af53a5a354cc388e9d537ddbfaf8e298f12
wirert <wirert@students.softuni.bg>
2022-09-23 12:20:39 +0300

    2

---
+++ 8.Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] countSets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < countSets[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < countSets[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            firstSet.IntersectWith(secondSet);
+
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+    }
+}
