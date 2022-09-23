a0806e2b8cdb38e2dda6c8a918ed07ef2c5d1c94
wirert <wirert@students.softuni.bg>
2022-09-23 12:45:23 +0300

    3

---
+++ 8.Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInputs = int.Parse(Console.ReadLine());
+
+            SortedSet<string> periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < numInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var chemical in input)
+                {
+                    periodicTable.Add(chemical);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
