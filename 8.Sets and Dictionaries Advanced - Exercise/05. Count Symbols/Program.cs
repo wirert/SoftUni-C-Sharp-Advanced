052427f806223df086e84a97014dedf63155c019
wirert <wirert@students.softuni.bg>
2022-09-23 13:00:58 +0300

    5

---
+++ 8.Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+
+            SortedDictionary<char, int> lettersCount = new SortedDictionary<char, int>();
+
+            foreach (var symbol in inputText)
+            {
+                if (!lettersCount.ContainsKey(symbol))
+                {
+                    lettersCount.Add(symbol, 0);
+                }
+
+                lettersCount[symbol]++;
+            }
+
+            foreach (var symbol in lettersCount)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
