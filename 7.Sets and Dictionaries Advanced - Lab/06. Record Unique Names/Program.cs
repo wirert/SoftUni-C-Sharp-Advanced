e5817f6443c7aae6ae0d4da6542e849a475f5492
wirert <wirert@students.softuni.bg>
2022-09-23 11:20:23 +0300

    6

---
+++ 7.Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNamesCollection = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                uniqueNamesCollection.Add(Console.ReadLine());
+            }
+
+            foreach (var name in uniqueNamesCollection)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
