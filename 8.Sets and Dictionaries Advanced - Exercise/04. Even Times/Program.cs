e36916d31d4e4cc0695348cf80b5f273dd7ddc1c
wirert <wirert@students.softuni.bg>
2022-09-23 12:55:06 +0300

    4

---
+++ 8.Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numberAppearing = new Dictionary<int, int>();
+
+            int inputsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputsCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numberAppearing.ContainsKey(number))
+                {
+                    numberAppearing.Add(number, 0);
+                }
+
+                numberAppearing[number]++;
+            }
+
+            Console.WriteLine(numberAppearing.Single(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
