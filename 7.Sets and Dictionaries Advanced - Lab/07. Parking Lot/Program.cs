9ecee4b2cc73df2afa9d016b5689c41c56b7db22
wirert <wirert@students.softuni.bg>
2022-09-23 11:28:29 +0300

    7

---
+++ 7.Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string[] command = Console.ReadLine().Split(", ");
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "IN")
+                {
+                    parking.Add(command[1]);
+                }
+                else
+                {
+                    if (parking.Contains(command[1]))
+                        parking.Remove(command[1]);
+                }
+
+                command = Console.ReadLine().Split(", ");
+            }
+
+            if (!parking.Any())
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
