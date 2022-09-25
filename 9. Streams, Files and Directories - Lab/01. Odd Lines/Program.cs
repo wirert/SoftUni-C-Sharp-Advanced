5ea2cf0c2f8b2109d1716b437ef07809eee969bb
wirert <wirert@students.softuni.bg>
2022-09-25 16:50:48 +0300

    OddLines and LineNumbers

---
+++ 9. Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+
+                int counter = 0;
+
+                using (writer)
+                {
+
+                    while (line != null)
+                    {
+
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 9. Streams, Files and Directories - Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            
+            using (reader)
+            {
+                int lineNumber = 1;
+
+                string line = null;
+
+                using (writer)
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{lineNumber++}. {line}");
+                    }
+                }
+            }
+        }
+    }
+}
