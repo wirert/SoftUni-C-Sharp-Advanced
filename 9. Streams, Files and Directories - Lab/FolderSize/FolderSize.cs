802d6a376b28dd6d00b93691325a2e235616e8c2
wirert <wirert@students.softuni.bg>
2022-09-27 16:28:10 +0300

    7. FolderSize

---
+++ 9. Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,33 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo folderInfo = new DirectoryInfo(folderPath);
+
+            FileInfo[] filesInfo = folderInfo.GetFiles("*", SearchOption.AllDirectories);
+
+            double size = 0;
+
+            foreach (var file in filesInfo)
+            {
+                size += file.Length;
+            }
+
+            size = size / 1024;
+
+            File.WriteAllText(outputFilePath, size.ToString());
+        }
+    }
+}
