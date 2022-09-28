8d13f9d604e8a7df17d63e6363626aedec4433d2
wirert <wirert@students.softuni.bg>
2022-09-28 13:20:43 +0300

    3. task

---
+++ 11.Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isStartWithUpper = w => char.IsUpper(w[0]);
+
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isStartWithUpper)
+                .ToArray()));
+        }
+    }
+}
