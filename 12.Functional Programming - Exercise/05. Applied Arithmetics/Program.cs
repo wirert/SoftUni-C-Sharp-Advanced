b843452f0e15ebebfcd5080849dd57ab296a9961
wirert <wirert@students.softuni.bg>
2022-09-28 18:32:12 +0300

    task 5

---
+++ 12.Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, int> calculate = (a, c) =>
+            {
+                switch (a)
+                {
+                    case "add": return c + 1;
+                    case "subtract": return c - 1;
+                    case "multiply": return c * 2;
+                    default:
+                        Console.Write($"{c} ");
+                        return c;
+                }
+            };
+
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            string action = null;
+
+            while ((action = Console.ReadLine()) != "end")
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    input[i] = calculate(action, input[i]);
+                }
+
+                if (action == "print")
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
