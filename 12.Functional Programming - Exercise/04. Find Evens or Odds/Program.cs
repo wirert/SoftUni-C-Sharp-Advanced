05669f054afd2f120d70be0ef35da0576886ffff
wirert <wirert@students.softuni.bg>
2022-09-28 17:19:47 +0300

    4.EvenOdd

---
+++ 12.Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string filterString = Console.ReadLine().Trim();
+
+            Predicate<string> isEvenCondition = w => w == "even";
+            Predicate<int> isEvenOrOdd = n => n % 2 == 0;
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (isEvenCondition(filterString))
+                {
+                    if (isEvenOrOdd(i))
+                        Console.Write($"{i} ");
+                }
+                else
+                {
+                    if (!isEvenOrOdd(i))
+                        Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
