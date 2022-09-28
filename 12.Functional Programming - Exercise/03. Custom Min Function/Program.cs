298d06632c739e8db5aee1ec7cf04c9f3358522e
wirert <wirert@students.softuni.bg>
2022-09-28 16:34:59 +0300

    3. MinValue

---
+++ 12.Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minNum = x => x.Min();
+
+            Console.WriteLine(minNum(Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray()));
+        }
+    }
+}
