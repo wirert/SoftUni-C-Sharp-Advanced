1939392ebfa8a7b676ccc11837c4210c1e9f9715
wirert <wirert@students.softuni.bg>
2022-09-28 18:57:46 +0300

    task 7

---
+++ 12.Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isLengthEnough = (w, l) => w.Length <= l;
+
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(Environment.NewLine, Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => isLengthEnough(w, length))
+                ));
+        }
+    }
+}
