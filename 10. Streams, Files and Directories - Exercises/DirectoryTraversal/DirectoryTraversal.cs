e1107c89a0b9901053f274dcd8df541f303e7a05
wirert <wirert@students.softuni.bg>
2022-09-28 09:56:00 +0300

    4.DirectoryTraversal

--- 10. Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
+++ 10. Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
@@ -1,4 +1,9 @@
-﻿namespace DirectoryTraversal
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DirectoryTraversal
 {
     using System;
     public class DirectoryTraversal
@@ -6,6 +11,7 @@
         static void Main()
         {
             string path = Console.ReadLine();
+            
             string reportFileName = @"\report.txt";
 
             string reportContent = TraverseDirectory(path);
@@ -16,12 +22,42 @@
 
         public static string TraverseDirectory(string inputFolderPath)
         {
-            throw new NotImplementedException();
+            DirectoryInfo dirInfo = new DirectoryInfo(inputFolderPath);
+
+            FileInfo[] files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
+
+            SortedDictionary<string, List<FileInfo>> extensionFiles = new SortedDictionary<string, List<FileInfo>>();
+
+            foreach (var fileInfo in files)
+            {
+                if (!extensionFiles.ContainsKey(fileInfo.Extension))
+                {
+                    extensionFiles.Add(fileInfo.Extension, new List<FileInfo>());
+                }
+
+                extensionFiles[fileInfo.Extension].Add(fileInfo);
+            }
+
+            StringBuilder text = new StringBuilder();
+
+            foreach (var extension in extensionFiles.OrderByDescending(e => e.Value.Count))
+            {
+                text.AppendLine(extension.Key);
+
+                foreach (var file in extension.Value.OrderBy(f => f.Length))
+                {
+                    text.AppendLine($"--{file.Name} - {file.Length / 1024}kb");
+                }
+            }
+            
+            return text.ToString();
         }
 
         public static void WriteReportToDesktop(string textContent, string reportFileName)
         {
-            throw new NotImplementedException();
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+
+            File.WriteAllText(filePath, textContent);
         }
     }
 }
