1dd845a0ae2939d71efbd3238b7e1f6b3a467858
wirert <wirert@students.softuni.bg>
2022-09-28 10:54:00 +0300

    5.CopyDirectory

--- 10. Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
+++ 10. Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
@@ -1,11 +1,13 @@
-﻿namespace CopyDirectory
+﻿using System.IO;
+
+namespace CopyDirectory
 {
     using System;
     public class CopyDirectory
     {
         static void Main()
         {
-            string inputPath =  @$"{Console.ReadLine()}";
+            string inputPath = @$"{ Console.ReadLine()}";
             string outputPath = @$"{Console.ReadLine()}";
 
             CopyAllFiles(inputPath, outputPath);
@@ -13,7 +15,19 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(outputPath))
+                Directory.Delete(outputPath, true);
+
+            Directory.CreateDirectory(outputPath);
+
+            foreach (var file in Directory.GetFiles(inputPath))
+            {
+                string fileName = Path.GetFileName(file);
+
+                string filePath = Path.Combine(outputPath, fileName);
+
+                File.Copy(file, filePath);
+            }
         }
     }
 }
