4f55eb37e7af356d9a56f8b4b8664edb6d85c109
wirert <wirert@students.softuni.bg>
2022-09-28 19:17:32 +0300

    task 8

---
+++ 12.Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Action<int, int[]> PrintIfDivisible = (i, nums) =>
+            {
+                if (nums.Any(num => i % num != 0))
+                {
+                    return;
+                }
+
+                Console.Write($"{i} ");
+            };
+
+            for (int i = 1; i <= range; i++)
+            {
+                PrintIfDivisible(i, dividers);
+            }
+        }
+    }
+}
