f511fbdfab27d8af0695e9f2f9d9989cf3ae1ec6
wirert <wirert@students.softuni.bg>
2022-09-28 12:43:10 +0300

    2. SumNumbers

---
+++ 11.Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
