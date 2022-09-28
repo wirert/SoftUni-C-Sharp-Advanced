7753366b6d594b1888008cf7aca4ca0319f9ed4e
wirert <wirert@students.softuni.bg>
2022-09-28 16:16:07 +0300

    1.ActionPrint

---
+++ 12.Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = p => Console.WriteLine(String.Join(Environment.NewLine, p));
+
+            print(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
