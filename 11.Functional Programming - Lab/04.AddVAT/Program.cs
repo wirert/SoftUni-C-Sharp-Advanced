23dd1418ad9e7bb93cb29b6345f6b8e69f497d91
wirert <wirert@students.softuni.bg>
2022-09-28 13:33:06 +0300

    4. AddVAT

---
+++ 11.Functional Programming - Lab/04.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> addVAT = n => (double.Parse(n) * 1.2).ToString("f2");
+
+            Console.WriteLine(string.Join(Environment.NewLine,
+                Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(addVAT)));
+        }
+    }
+}
