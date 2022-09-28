406fe088f14b925d9cf59dc38979e9f718b12373
wirert <wirert@students.softuni.bg>
2022-09-28 16:25:41 +0300

    2.KnightsOfHonor

---
+++ 12.Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printSir = a => Console.WriteLine(string
+                .Join(Environment.NewLine, a
+                    .Select(w => $"Sir {w}")));
+
+            printSir(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
