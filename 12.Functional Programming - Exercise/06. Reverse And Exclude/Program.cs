33f8b3d8d98ab15aabefc160fbf5529b041a4a61
wirert <wirert@students.softuni.bg>
2022-09-28 18:45:37 +0300

    task 6

---
+++ 12.Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+
+            int num = int.Parse(Console.ReadLine());
+
+            Func<int, int[], int[]> ifDivisible = (n, nums) =>
+            {
+                return nums.Where(x => x % n != 0).ToArray();
+            };
+
+            Console.WriteLine(string.Join(' ', ifDivisible(num, input)));
+        }
+    }
+}
