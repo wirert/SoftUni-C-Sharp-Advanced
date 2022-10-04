5dcf5b1990b5e99f0cca2b5ba9e63df4b10123a2
wirert <wirert@students.softuni.bg>
2022-10-04 18:01:15 +0300

    5.DateModifier

---
+++ 14.Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static int DifferenceBetweenTwoDates(string startDateString, string endDateString)
+        {
+            DateTime startDate = DateTime.Parse(startDateString);
+            DateTime endDate = DateTime.Parse(endDateString);
+
+            TimeSpan timeSpan = endDate - startDate;
+
+            return Math.Abs(timeSpan.Days);
+        }
+    }
+}
