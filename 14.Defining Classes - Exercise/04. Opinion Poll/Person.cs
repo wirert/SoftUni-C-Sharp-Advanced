f03c58e2f08c491db7d2d4bfb71b3f742b307c14
wirert <wirert@students.softuni.bg>
2022-10-04 17:38:38 +0300

    04.Opinion_Poll

---
+++ 14.Defining Classes - Exercise/04. Opinion Poll/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Opinion_Poll
+{
+    public class Person
+    {
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ 14.Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Opinion_Poll
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                if (age > 30)
+                {
+                    people.Add(new Person(name, age));
+                }
+            }
+
+            foreach (var person in people.OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
