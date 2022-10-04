1ff442490120abb5767763cd5e3de2a78202beb6
wirert <wirert@students.softuni.bg>
2022-10-04 17:13:28 +0300

    3.OldestFamilyMember

---
+++ 14.Defining Classes - Exercise/DefiningClasses/Family.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+    }
+}
--- 14.Defining Classes - Exercise/DefiningClasses/StartUp.cs
+++ 14.Defining Classes - Exercise/DefiningClasses/StartUp.cs
@@ -6,7 +6,22 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            
+            int numPeople = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numPeople; i++)
+            {
+                string[] inputPerson = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(inputPerson[0], int.Parse(inputPerson[1]));
+
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
         }
     }
 }
