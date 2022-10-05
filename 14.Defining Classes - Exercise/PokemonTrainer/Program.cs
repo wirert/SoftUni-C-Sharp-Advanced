949e54b3541a6b231719c709eba877bdb2fa91a6
wirert <wirert@students.softuni.bg>
2022-10-05 17:22:08 +0300

    9.PokemonTrainer

---
+++ 14.Defining Classes - Exercise/PokemonTrainer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Tournament") break;
+
+                Pokemon pokemon = new Pokemon(command[1], command[2], int.Parse(command[3]));
+
+                Trainer trainer = trainers.SingleOrDefault(t => t.Name == command[0]);
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(command[0], 0);
+                    trainers.Add(trainer);
+                }
+
+                trainer.Pokemons.Add(pokemon);
+            }
+
+            string input = null;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Any(p => p.Element == input))
+                    {
+                        trainer.Badges++;
+                    }
+                    else
+                    {
+                        trainer.DecreaseHealth();
+                    }
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(t => t.Badges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+        }
+    }
+}
+
---
+++ 14.Defining Classes - Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, int badges)
+        {
+            Name = name;
+            Badges = badges;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void DecreaseHealth()
+        {
+            for (int i = 0; i < Pokemons.Count; i++)
+            {
+                Pokemons[i].Health -= 10;
+
+                if (Pokemons[i].Health <= 0)
+                {
+                    Pokemons.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
