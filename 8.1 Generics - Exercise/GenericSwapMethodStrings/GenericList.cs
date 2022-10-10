d14d8af1355ad051d98a0fe5c66a6daa932a8413
wirert <wirert@students.softuni.bg>
2022-10-10 18:04:51 +0300

    GenericSwapMethodStrings

---
+++ 8.1 Generics - Exercise/GenericSwapMethodStrings/GenericList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    internal class GenericList<T>
+    {
+        public GenericList()
+        {
+            List = new List<T>();
+        }
+
+        public List<T> List { get; set; }
+
+        public void Swap(int index1, int index2)
+        {
+            if (index1 < 0 || index1 >= this.List.Count || index2 < 0 || index2 >= this.List.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T copy = List[index1];
+            List[index1] = List[index2];
+            List[index2] = copy;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in this.List)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 8.1 Generics - Exercise/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GenericList<string> list = new GenericList<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.List.Add(Console.ReadLine());
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            list.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(list);
+        }
+    }
+}
