2f9d6cc0c95634f44757536d42df79497a7d703d
wirert <wirert@students.softuni.bg>
2022-10-10 16:57:49 +0300

    GenericArrayCreator

---
+++ 08. Generics - Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(10, "daf");
+
+            int[] ints = ArrayCreator.Create(20, 5);
+
+            Console.WriteLine(string.Join(',', strings));
+            Console.WriteLine(string.Join(',', ints));
+        }
+    }
+}
