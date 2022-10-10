7c6f1130b4cbc60f5bcd0fffc288a7fc9d44426f
wirert <wirert@students.softuni.bg>
2022-10-10 17:37:17 +0300

    GenericBoxOfInteger

---
+++ 8.1 Generics - Exercise/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfInteger
+{
+    internal class Box<T> where T : struct
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ 8.1 Generics - Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfString
+{
+    internal class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
