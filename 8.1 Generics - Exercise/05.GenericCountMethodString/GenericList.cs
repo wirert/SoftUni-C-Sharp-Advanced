1c8d51ce7df2da3976b95535438c97de7347cc9c
wirert <wirert@students.softuni.bg>
2022-10-10 19:26:57 +0300

    05.GenericCountMethodString

---
+++ 8.1 Generics - Exercise/05.GenericCountMethodString/GenericList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodString
+{
+    public class GenericList<T> where T : IComparable
+    {
+        public GenericList()
+        {
+            List = new List<T>();
+        }
+
+        public List<T> List { get; set; }
+
+        public void Swap(int index1, int index2)
+        {
+            if (index1 < 0 || index1 >= this.List.Count || index2 < 0 || index2 >= this.List.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T copy = List[index1];
+            List[index1] = List[index2];
+            List[index2] = copy;
+        }
+
+        public int CompareAndCount(T elementToCompareWith)
+        {
+            int count = 0;
+
+            foreach (var item in this.List)
+            {
+                if (item.CompareTo(elementToCompareWith) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in this.List)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 8.1 Generics - Exercise/05.GenericCountMethodString/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.GenericCountMethodString
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GenericList<string> list = new GenericList<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.List.Add(Console.ReadLine());
+            }
+
+            string elementToCompare = Console.ReadLine();
+
+            Console.WriteLine(list.CompareAndCount(elementToCompare));
+        }
+    }
+}
