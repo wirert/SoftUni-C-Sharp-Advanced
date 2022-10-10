f0de51d6bc9e6db9f7aab5ede624243d2aa8e2d1
wirert <wirert@students.softuni.bg>
2022-10-10 16:13:42 +0300

    1. Box of T

---
+++ 17.Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+	internal class Box<T>
+    {
+		private Stack<T> elements;
+
+		public Box()
+		{
+			this.elements = new Stack<T>();
+		}
+
+		public int Count { get { return elements.Count; } }
+
+		public void Add(T element)
+		{
+			this.elements.Push(element);
+		}
+
+		public T Remove()
+		{
+			return this.elements.Pop();
+		}
+	}
+}
