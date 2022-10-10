f06c2575fad406c67eed8e786d5aebaa7758dca1
wirert <wirert@students.softuni.bg>
2022-10-10 17:12:26 +0300

    GenericScale

---
+++ 08. Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<string> scale = new EqualityScale<string>("asd","asd");
+
+            Console.WriteLine( scale.AreEqual());
+        }
+    }
+}
