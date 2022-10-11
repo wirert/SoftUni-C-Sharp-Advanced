6e5e58bdb2717ed0774f4793cded73eef3d57ed1
wirert <wirert@students.softuni.bg>
2022-10-11 17:53:02 +0300

    07.Tuple

---
+++ 8.1 Generics - Exercise/07.Tuple/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            string[] nameInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] drinkInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] numsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string address = nameInput.Length == 4 ? $"{nameInput[2]} {nameInput[3]}" : nameInput[2];
+
+            Tuple<string, string> nameAddress = new Tuple<string, string>
+                ($"{nameInput[0]} {nameInput[1]}", address);
+
+            Tuple<string, int> nameDrinks = new Tuple<string, int>
+               (drinkInput[0], int.Parse(drinkInput[1]));
+
+            Tuple<int, double> nums = new Tuple<int, double>
+               (int.Parse(numsInput[0]), double.Parse(numsInput[1]));
+
+            Console.WriteLine(nameAddress);
+            Console.WriteLine(nameDrinks);
+            Console.WriteLine(nums);
+        }
+    }
+}
