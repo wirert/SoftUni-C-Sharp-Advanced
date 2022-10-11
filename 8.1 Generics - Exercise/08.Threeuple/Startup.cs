3c397158805be505c6cef922cce2f4c5f0b13b82
wirert <wirert@students.softuni.bg>
2022-10-11 23:38:21 +0300

    08.Threeuple

---
+++ 8.1 Generics - Exercise/08.Threeuple/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.Threeuple
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            string[] nameInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] drinkInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] numsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        
+            string town = nameInput.Length == 5 ? $"{nameInput[3]} {nameInput[4]}" : nameInput[3];
+
+            Threeuple<string, string, string> nameAddress = new Threeuple<string, string, string>
+                ($"{nameInput[0]} {nameInput[1]}", nameInput[2], town);
+
+            Threeuple<string, int, bool> nameDrinks = new Threeuple<string, int, bool>
+               (drinkInput[0], int.Parse(drinkInput[1]), drinkInput[2] == "drunk");
+
+            Threeuple<string, double, string> bankAccount = new Threeuple<string, double, string>
+               (numsInput[0], double.Parse(numsInput[1]), numsInput[2]);
+
+            Console.WriteLine(nameAddress);
+            Console.WriteLine(nameDrinks);
+            Console.WriteLine(bankAccount);
+        }
+    }
+}
