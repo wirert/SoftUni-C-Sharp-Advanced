80e464d4fbf2014d23b0e0e39441861d757937a1
wirert <wirert@students.softuni.bg>
2022-10-15 14:25:03 +0300

    03.Basketball (93 from 100 points)

---
+++ Exam Exercises/Retake Exam – 18 August 2022/03. Basketball/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Basketball
+{
+    public class Player
+    {
+        public Player(double rating, int games)
+        {
+            Rating = rating;
+            Games = games;
+            Retired = false;
+        }
+
+        public Player(string name, double rating, int games) : this(rating, games)
+        {
+            Name = name;
+        }
+        
+        public Player(string name, string position, double rating, int games) : this(name,rating,games)
+        {
+            Position = position;           
+        }
+
+        public string Name { get; private set; }
+        public string Position { get; private set; }
+        public double Rating { get; private set; }
+        public int Games { get; private set; }
+        public bool Retired { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"-Player: {Name}");
+            sb.AppendLine($"--Position: {Position}");
+            sb.AppendLine($"--Rating: {Rating}");
+            sb.AppendLine($"--Games played: {Games}");
+
+            return sb.ToString().TrimEnd(); 
+        }
+    }
+}
---
+++ Exam Exercises/Retake Exam – 18 August 2022/03. Basketball/Team.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basketball
+{
+    public class Team
+    {
+        private List<Player> players;
+
+        public Team(string name, int openPositions, char group)
+        {
+            Name = name;
+            OpenPositions = openPositions;
+            Group = group;
+            this.players = new List<Player>();
+        }
+
+        public string Name { get; private set; }
+        public int OpenPositions { get; private set; }
+        public char Group { get; private set; }
+        public int Count { get { return players.Count; } }
+
+        public string AddPlayer(Player player)
+        {
+            if (string.IsNullOrEmpty(player.Name) || string.IsNullOrEmpty(player.Position))
+            {
+                return "Invalid player's information.";
+            }
+
+            if (OpenPositions == 0)
+            {
+                return "There are no more open positions.";
+            }
+
+            if (player.Rating < 80)
+            {
+                return "Invalid player's rating.";
+            }
+
+            players.Add(player);
+            OpenPositions--;
+
+            return $"Successfully added {player.Name} to the team. Remaining open positions: {OpenPositions}.";
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            var player = players.FirstOrDefault(p => p.Name == name);
+
+            if (player != null)
+            {
+                players.Remove(player);
+                OpenPositions++;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public int RemovePlayerByPosition(string position)
+        {
+            int removedPlayers = players.RemoveAll(p => p.Position == position);
+            OpenPositions -= removedPlayers;
+
+            return removedPlayers;
+        }
+
+        public Player RetirePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(p => p.Name == name);
+
+            if (player != null)
+            {
+                player.Retired = true;
+            }
+
+            return player;
+        }
+
+        public List<Player> AwardPlayers(int games) => players.FindAll(p => p.Games >= games);
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Active players competing for Team {Name} from Group {Group}:");
+
+            foreach (var player in players.Where(p => p.Retired == false))
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
