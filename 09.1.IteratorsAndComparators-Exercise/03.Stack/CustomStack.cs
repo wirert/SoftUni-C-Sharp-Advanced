97d6e7acce6f667a54230eb65a7f242a3264d82f
wirert <wirert@students.softuni.bg>
2022-10-15 09:34:43 +0300

    _03.Stack

---
+++ 09.1.IteratorsAndComparators-Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class CustomStack : IEnumerable
+    {
+        private const int initialCapacity = 4;
+        private int[] stack;
+
+        public CustomStack()
+        {
+            this.stack = new int[initialCapacity];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(params int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                if (stack.Length == Count)
+                {
+                    Resize();
+                }
+
+                stack[Count] = num;
+                Count++;
+            }
+        }
+
+        public int Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            Count--;
+            int element = stack[Count];
+
+            return element;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[stack.Length * 2];
+
+            for (int i = 0; i < stack.Length; i++)
+            {
+                copy[i] = stack[i];
+            }
+
+            stack = copy;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return stack[i];
+            }
+        }
+    }
+}
---
+++ 09.1.IteratorsAndComparators-Exercise/03.Stack/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            int[] inputElements = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(int.Parse)
+                .ToArray();
+
+            stack.Push(inputElements);
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "END")
+            {
+                switch (command[0])
+                {
+                    case "Push":
+                        stack.Push(command.Skip(1).Select(int.Parse).ToArray());
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (InvalidOperationException exeption)
+                        {
+
+                            Console.WriteLine(exeption.Message);
+                        }
+
+                        break;
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (stack.Count == 0) return;
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
