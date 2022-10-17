aa451e4e85e90017e261fa42dc2fa5911b96058f
wirert <wirert@students.softuni.bg>
2022-10-17 15:05:56 +0300

    01.BirthdayCelebration

---
+++ Exam Exercises/RetakeExam.18August2021/01.BirthdayCelebration/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guests = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray().Reverse());
+            var plates = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            int wastedFood = 0;
+            
+
+            while (guests.Count != 0 && plates.Count != 0)
+            {
+                int plate = plates.Pop();
+                int guest = guests.Pop();                
+
+                if (plate >= guest)
+                {
+                    wastedFood += plate - guest;                    
+                }
+                else
+                {
+                    guest -= plate;
+                    guests.Push(guest);
+                }
+            }
+
+            Console.WriteLine(plates.Count == 0 ? $"Guests: {string.Join(" ", guests)}"
+                : $"Plates: {string.Join(" ", plates.Reverse())}");
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
